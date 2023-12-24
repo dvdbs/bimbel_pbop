cde57e4f84253261e91f5709d828afe4496ae433
dvdbs <127390679+dvdbs@users.noreply.github.com>
2023-12-24 19:50:56 +0700

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,37 +1,144 @@
-pelajar ='''CREATE TABLE IF NOT EXISTS Pelajar(
-   Id_pelajar int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+siswa = '''CREATE TABLE IF NOT EXISTS Siswa(
+   Id_siswa int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Id_paket_belajar int(10),
+   FOREIGN KEY (Id_paket_belajar) REFERENCES paket_belajar(Id_paket_belajar) ON DELETE CASCADE,
    Nama varchar(200) NOT NULL,
    Email varchar(200) NOT NULL UNIQUE,
    Nomor varchar(30) NOT NULL UNIQUE,
-   Jenjang_pendidikan ENUM('SD', 'SMP', 'SMA') NOT NULL,
+   Kelas ENUM('SD', 'SMP', 'SMA') NOT NULL,
    Jenis_kelamin ENUM('Laki-laki', 'Perempuan') NOT NULL,
-   Password varchar(200) not null
+   Alamat varchar(200) NOT NULL
+	);
+'''
+
+guru ='''CREATE TABLE IF NOT EXISTS Guru(
+   Id_guru int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Nama varchar(200) NOT NULL,
+   Email varchar(200) NOT NULL UNIQUE,
+   Nomor varchar(30) NOT NULL UNIQUE,
+   Jenis_kelamin ENUM('Laki-laki', 'Perempuan') NOT NULL,
+   Tgl_lahir varchar(30) NOT NULL,
+   Alamat varchar(200) NOT NULL,
+   Status_pekerja ENUM('Kontrak', 'Tetap') NOT NULL,
+   Bidang_mapel ENUM('Matematika', 'Bahasa Indonesia', 'Bahasa Inggris', 'Ilmu Pengetahuan Alam', 'Biologi', 'Kimia', 'Fisika', 'Geografi', 'Ekonomi', 'Sosiologi', 'Sejarah') NOT NULL,
+   Gaji varchar(100) NOT NULL
     );
 '''
 
-pengajar ='''CREATE TABLE IF NOT EXISTS Pengajar(
-   Id_pengajar int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+pegawai = '''CREATE TABLE IF NOT EXISTS Pegawai(
+   Id_pegawai int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
    Nama varchar(200) NOT NULL,
    Email varchar(200) NOT NULL UNIQUE,
    Nomor varchar(30) NOT NULL UNIQUE,
-   Usia varchar(30) NOT NULL,
    Jenis_kelamin ENUM('Laki-laki', 'Perempuan') NOT NULL,
-   Password varchar(200) not null
+   Tgl_lahir varchar(30) NOT NULL,
+   Alamat varchar(200) NOT NULL,
+   Status_pekerja ENUM('Kontrak', 'Tetap') NOT NULL,
+   Jabatan ENUM('OB', 'Admin', 'Manager') NOT NULL,
+   Tunjangan varchar(100) NOT NULL,
+   Gaji varchar(100) NOT NULL
     );
 '''
 
-admin = '''CREATE TABLE IF NOT EXISTS Admin(
-   Id_admin int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   Nama varchar(200) NOT NULL,
-   Email varchar(200) NOT NULL UNIQUE,
-   Nomor varchar(30) NOT NULL UNIQUE,
-   Usia varchar(200) not null,
-   Jenis_kelamin ENUM('Laki-laki', 'Perempuan') NOT NULL
+absen_pegawai = '''CREATE TABLE IF NOT EXISTS Absen_pegawai (
+   Id_absen_pegawai int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Id_pegawai int(10),
+   FOREIGN KEY (Id_pegawai) REFERENCES pegawai(Id_pegawai) ON DELETE CASCADE,
+   Tanggal date,
+   Jam_datang time,
+   Jam_selesai time,
+   Absen ENUM('Hadir', 'Izin', 'Sakit', 'Alpha') NOT NULL
+    );
+'''
+
+transaksi = '''CREATE TABLE IF NOT EXISTS Transaksi (
+   Id_transaksi int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Id_pegawai int(10),
+   FOREIGN KEY (Id_pegawai) REFERENCES pegawai(Id_pegawai) ON DELETE CASCADE,
+   Id_siswa int(10),
+   FOREIGN KEY (Id_siswa) REFERENCES siswa(Id_siswa) ON DELETE CASCADE,
+   Id_paket_belajar int(10),
+   FOREIGN KEY (Id_paket_belajar) REFERENCES paket_belajar(Id_paket_belajar) ON DELETE CASCADE,
+   Jenis_pembayaran ENUM('Cash', 'Transfer') NOT NULL,
+   Bayar int(100) NOT NULL,
+   Kembalian int(100) NOT NULL,
+   Tgl_transaksi date
+    );
+'''
+
+absen_siswa = '''CREATE TABLE IF NOT EXISTS Absen_siswa (
+   Id_absen_siswa int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Id_guru int(10),
+   FOREIGN KEY (Id_guru) REFERENCES guru(Id_guru) ON DELETE CASCADE,
+   Id_siswa int(10),
+   FOREIGN KEY (Id_siswa) REFERENCES siswa(Id_siswa) ON DELETE CASCADE,
+   Id_jadwal int(10), 
+   FOREIGN KEY (Id_jadwal) REFERENCES jadwal(Id_jadwal) ON DELETE CASCADE,
+   Tanggal date,
+   Absen ENUM('Hadir', 'Izin', 'Sakit', 'Alpha') NOT NULL
+    );
+'''
+
+jadwal = '''CREATE TABLE IF NOT EXISTS Jadwal (
+   Id_jadwal int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Id_guru int(10),
+   FOREIGN KEY (Id_guru) REFERENCES guru(Id_guru) ON DELETE CASCADE,
+   Kelas ENUM('SD', 'SMP', 'SMA') NOT NULL,
+   Mapel ENUM('Matematika', 'Bahasa Indonesia', 'Bahasa Inggris', 'Ilmu Pengetahuan Alam', 'Biologi', 'Kimia', 'Fisika', 'Geografi', 'Ekonomi', 'Sosiologi', 'Sejarah') NOT NULL,
+   Jam time,
+   Tanggal date,
+   Id_ruangan int(10) NOT NULL,
+   Paket_belajar ENUM('Reguler', 'Premium') NOT NULL
+    );
+'''
+
+ruangan ='''CREATE TABLE IF NOT EXISTS Ruangan(
+   Id_ruangan int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Kondisi_ruangan ENUM('Layak', 'Tidak layak') NOT NULL,
+   Kapasitas_kursi int(10) NOT NULL
+    );
+'''
+
+absen_guru = '''CREATE TABLE IF NOT EXISTS Absen_guru (
+   Id_absen_guru int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Id_guru int(10),
+   FOREIGN KEY (Id_guru) REFERENCES guru(Id_guru) ON DELETE CASCADE,
+   Tanggal date,
+   Jam_datang time,
+   Jam_selesai time,
+   Absen ENUM('Hadir', 'Izin', 'Sakit', 'Alpha') NOT NULL
+    );
+'''
+
+jadwal_pelayanan = '''CREATE TABLE IF NOT EXISTS Jadwal_pelayanan (
+   Id_jadwal_pelayanan int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Id_guru int(10),
+   FOREIGN KEY (Id_guru) REFERENCES guru(Id_guru) ON DELETE CASCADE,
+   Mapel ENUM('Matematika', 'Bahasa Indonesia', 'Bahasa Inggris', 'Ilmu Pengetahuan Alam', 'Biologi', 'Kimia', 'Fisika', 'Geografi', 'Ekonomi', 'Sosiologi', 'Sejarah') NOT NULL,
+   Jam_pelayanan time,
+   Id_ruangan int(10),
+   FOREIGN KEY (Id_ruangan) REFERENCES ruangan(Id_ruangan) ON DELETE CASCADE
+    );
+'''
+
+paket_belajar ='''CREATE TABLE IF NOT EXISTS Paket_belajar(
+   Id_paket_belajar int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   Kelas ENUM('SD', 'SMP', 'SMA') NOT NULL,
+   Kategori ENUM('Reguler', 'Premium') NOT NULL,
+   Biaya int(100)
     );
 '''
 
 def created_table(db) :
-    db.create(pelajar)
-    db.create(pengajar)
-    db.create(admin)
+    db.create(siswa)
+    db.create(guru)
+    db.create(pegawai)
+    db.create(absen_pegawai)
+    db.create(transaksi)
+    db.create(absen_siswa)
+    db.create(jadwal)
+    db.create(ruangan)
+    db.create(absen_guru)
+    db.create(jadwal_pelayanan)
+    db.create(paket_belajar)
     print("Tabel berhasil dibuat")
